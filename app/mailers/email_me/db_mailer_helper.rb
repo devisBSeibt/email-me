63ee67c9671722363f28b1d9b1a11c8c3981f385
Annie Huddleston <ahuddleston@devis.com>
2016-05-16 11:17:45 -0400

    logging cleanup

--- app/mailers/email_me/db_mailer_helper.rb
+++ app/mailers/email_me/db_mailer_helper.rb
@@ -7,19 +7,11 @@ module EmailMe
     def mail_template_email(template_name, to, params)
 
       mail_template = EmailMe::MailTemplate.find_by name: template_name
-      Rails.logger.debug mail_template.content
-
       liquid_template = Liquid::Template.parse(mail_template.content)
-
       processed_template = liquid_template.render(params)
 
-      Rails.logger.debug processed_template
-
       html_mail = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(processed_template)
-      Rails.logger.debug html_mail
-
       plain_text_mail = Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(processed_template)
-      Rails.logger.debug plain_text_mail
 
       @body_html = html_mail.html_safe
       @body_plain = plain_text_mail
